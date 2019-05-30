8740c30762f5574d1c08668ef3270eff39edc5c3
spiperac <spiperac@denkei.org>
2019-05-30 14:57:04 +0200

    basic i3 config added

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 mkdir -p ~/.vim/bundle
-apt update && apt install cmake git python3 fonts-powerline curl zsh
+apt update && apt install cmake git python3 fonts-powerline curl zsh i3 i3status i3lock
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
 cp vimrc ~/.vimrc
 cp zshrc ~/.zshrc
+cp -R i3 ~/.config/
 vim +PluginInstall
 python3 ~/.vim/bundle/YouCompleteMe/install.py
