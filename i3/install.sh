5ee84d4e09ffba1bee667a4051fd658d8cf5f70e
Strahinja Piperac <spiperac@denkei.org>
2020-06-26 23:37:54 +0200

    Linking i3 and i3status added to install script.
    Signed-off-by: Strahinja Piperac <spiperac@denkei.org>

--- i3/install.sh
+++ i3/install.sh
@@ -5,3 +5,6 @@ sudo add-apt-repository ppa:kgilmer/speed-ricer -y
 sudo apt update
 
 sudo apt install i3-gaps -y
+
+ln -s $(pwd) /home/spiperac/.config/i3
+ln -s $(pwd)/i3status/ /home/spiperac/.config/i3status
