a08c7626686c5fe1eaf2021a95fc86e520a4ddc6
spiperac <spiperac@denkei.org>
2024-11-16 04:47:49 +0100

    Removed old unused stuff

--- setup.sh
+++ setup.sh
@@ -8,16 +8,8 @@ sudo apt install i3 i3lock i3status rofi dmenu
 cp -R i3 ~/.config/
 
 # Vim Installation
-sudo apt install vim fonts-powerline
-cp vimrc ~/.vimrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-mkdir -p ~/.vim/bundle
-# You complete me
-cp .ycm_extra_conf.py ~/.vim/.ycm_extra_conf.py
-python3 ~/.vim/bundle/YouCompleteMe/install.py
-vim +PlugInstall
+sudo apt install neovim
+cp -R nvim ~/.config/
 
 # ZSH Installation
 sudo apt install zsh
