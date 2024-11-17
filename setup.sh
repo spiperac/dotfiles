ad6673af4b286a8d1563cef3d14a69ae570781d4
spiperac <spiperac@denkei.org>
2024-11-17 16:44:13 +0100

    Updating 10 years old i3wm config with polybar and rufi

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+## Update and isntall packages
 sudo apt update
 sudo apt install curl git cmake python3 -y
+sudo apt install i3 i3lock polybar rofi feh -y
+sudo apt install python3 python3-pip
+sudo apt install zsh -y
+sudo apt install neovim nodejs -y
 
 ## Setup fonts
 FONTS_DIR="/home/spiperac/.local/share/fonts"
@@ -11,26 +16,21 @@ fc-cache -fv
 exit
 
 ## i3 Installation
-sudo apt install i3 i3lock i3status rofi dmenu -y
 ln -rs i3 ~/.config/i3
 
 ### Rust and Python setup\
 
 # Python
-sudo apt install python3 python3-pip
 pip install pylint black flake8
 
 # Rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 rustup component add rust-analyzer
 
-
-## nvim Installation
-sudo apt install neovim nodejs -y
+## nvim Setup
 ln -rs nvim ~/.config/nvim
 
-## ZSH Installation
-sudo apt install zsh -y
+## ZSH Setup
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s zshrc ~/.zshrc
 	
