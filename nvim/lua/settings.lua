f0007f52820a7d06b2f983d37b6641bcf91f1faa
spiperac <spiperac@denkei.org>
2024-11-17 03:19:07 +0100

    Updated treesitter with more hl configs

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -2,10 +2,24 @@
 
 -- General Settings
 vim.o.number = true -- Enable line numbers
-vim.o.relativenumber = true -- Enable relative line numbers
 vim.o.linespace = 0 -- No additional spacing between lines
 vim.o.clipboard = "unnamedplus" -- Use system clipboard
+vim.o.termguicolors = true            -- Enable true color support
+vim.o.expandtab = true                -- Use spaces instead of tabs
+vim.o.shiftwidth = 4                  -- Number of spaces for indentation
+vim.o.tabstop = 4                     -- Number of spaces for a tab
+vim.o.smartindent = true              -- Smart auto-indenting
+vim.o.autoindent = true               -- Maintain indentation level on new lines
+vim.opt.formatoptions:remove("cro")   -- Disable auto-comment on new lines
+
 
 -- Colorscheme
 vim.cmd("colorscheme gruvbox")
 
+-- Customize Neo-tree Background
+vim.cmd([[
+  highlight NeoTreeNormal guibg=#1d2021 guifg=#ebdbb2
+  highlight NeoTreeNormalNC guibg=#1d2021 guifg=#ebdbb2
+  highlight NeoTreeFloatBorder guibg=#1d2021 guifg=#665c54
+  highlight NeoTreeFloatTitle guibg=#1d2021 guifg=#b8bb26
+]])
