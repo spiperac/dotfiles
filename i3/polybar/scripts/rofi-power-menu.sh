2435332a79a2088a250dbbf1193fd57e72838d9f
spiperac <spiperac@denkei.org>
2024-11-17 21:16:42 +0100

    Added power menu for polybar

---
+++ i3/polybar/scripts/rofi-power-menu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+rofi_command="rofi -theme ~/.config/i3/rofi/launcher.rasi"
+
+# Options
+shutdown="  Shutdown"
+reboot="  Restart"
+lock="  Lock"
+logout="  Logout"
+
+# Confirmation
+confirm_exit() {
+    rofi -dmenu -i -no-fixed-num-lines -p "Are you sure? (yes/no)" -theme ~/.config/i3/rofi/launcher.rasi
+}
+
+# Variable passed to Rofi
+options="$lock\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -theme ~/.config/i3/rofi/launcher.rasi)"
+case $chosen in
+    $lock)
+        i3lock # Replace with your lock screen command
+        ;;
+    $logout)
+        confirm_exit && pkill -KILL -u $USER
+        ;;
+    $reboot)
+        confirm_exit && systemctl reboot
+        ;;
+    $shutdown)
+        confirm_exit && systemctl powe~/.config/i3/rofi/launcher.rasiroff
+        ;;
+esac
