a9aa75c0a91d4225ff3959c772c17189cdcb49da
spiperac <spiperac@denkei.org>
2024-11-18 06:12:42 +0100

    Modified weather script

--- i3/polybar/scripts/weather.sh
+++ i3/polybar/scripts/weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 LOCATION="Novi+Sad"
-WEATHER=$(curl -s "https://wttr.in/$LOCATION?format=%c+%t")
+WEATHER=$(curl -s "https://wttr.in/$LOCATION?format=1")
 
 if [ -z "$WEATHER" ]; then
     echo "N/A"
