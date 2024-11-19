df7833f78cf5cbe6716d587b06e9286ed8c6ca7f
spiperac <spiperac@denkei.org>
2024-11-19 02:01:53 +0100

    Icons in rufi fixed

--- i3/rofi/rofi.sh
+++ i3/rofi/rofi.sh
@@ -1,1 +1,1 @@
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/i3/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show drun -show-icons -modi drun -theme ~/.config/i3/rofi/launcher.rasi
