4c2aa1e6faeddad02fe876d2284ea06b5bade202
spiperac <spiperac@denkei.org>
2024-11-27 16:16:20 +0100

    fixed missing stuff for laptop and arch

--- setup.sh
+++ setup.sh
@@ -15,8 +15,9 @@ APT_DEPENDENCIES=(
 )
 
 PACMAN_DEPENDENCIES=(
-    curl git cmake python i3 i3lock rofi feh
-    pulseaudio alacritty polybar zsh neovim nodejs
+    curl git cmake python python-pip i3 i3lock rofi feh
+    alacritty polybar zsh neovim nodejs dunst adwaita-icon-theme
+    npm redshift
 )
 
 PYTHON_TOOLS=(
