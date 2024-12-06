5350c2a415f15d5cf623651f1c9f2936bb6c8447
spiperac <spiperac@denkei.org>
2024-12-06 19:15:24 +0100

    removed toggleterm

--- config/.config/nvim/init.lua
+++ config/.config/nvim/init.lua
@@ -14,7 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Load Lazy.nvim and plugins
 require("lazy").setup({
-  { "akinsho/toggleterm.nvim", config = require("plugins.toggleterm") },
   { "nvim-neo-tree/neo-tree.nvim", dependencies = { "nvim-lua/plenary.nvim", "MunifTanjim/nui.nvim" }, config = require("plugins.neo-tree") },
   { "lewis6991/gitsigns.nvim", config = require("plugins.gitsigns") },
   { "rafi/awesome-vim-colorschemes" },
@@ -22,7 +21,7 @@ require("lazy").setup({
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", config = require("plugins.treesitter") },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = require("plugins.telescope") },
   { "goolord/alpha-nvim" },
-  { "nvim-tree/nvim-web-devicons"},
+  { "nvim-tree/nvim-web-devicons" },
 })
 
 
