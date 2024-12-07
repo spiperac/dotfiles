f2f8538c83c42632f3fd5094a223d8f8f21e91c2
spiperac <spiperac@denkei.org>
2024-12-07 16:54:12 +0100

    minor fixes in nvim config

--- config/.config/nvim/lua/plugins/buffhunter.lua
+++ config/.config/nvim/lua/plugins/buffhunter.lua
@@ -1,4 +1,12 @@
 return function()
-	require("buffhunter").setup()
+    require('buffhunter').setup {
+      keymaps = {
+        open = '<CR>',         -- Open selected buffer
+        close = 'q',           -- Close selected buffer
+        delete = 'x',
+        split_h = 's',         -- Open in horizontal split
+        split_v = 'v',         -- Open in vertical split
+      },
+      git_signs = true,        -- Enable Git status display
+    }
 end
-
