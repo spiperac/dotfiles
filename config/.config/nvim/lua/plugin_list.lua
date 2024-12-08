ccb8da57cf19a9e838335b4079128f6b0d57f7f5
spiperac <spiperac@denkei.org>
2024-12-08 18:36:27 +0100

    Moved plugins into separate file

---
+++ config/.config/nvim/lua/plugin_list.lua
@@ -0,0 +1,70 @@
+-- Load Lazy.nvim and plugins
+require("lazy").setup({
+  { 
+    "nvim-neo-tree/neo-tree.nvim", 
+    dependencies = { 
+      "nvim-lua/plenary.nvim", 
+      "MunifTanjim/nui.nvim" 
+    }, 
+    config = require("plugins.neo-tree") 
+  },
+
+  { 
+    "lewis6991/gitsigns.nvim", 
+    config = require("plugins.gitsigns") 
+  },
+  
+  { "rafi/awesome-vim-colorschemes" },
+
+  { 
+    "neoclide/coc.nvim", 
+    branch = "release", 
+    config = require("plugins.coc") 
+  },
+  
+  { 
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate", 
+    config = require("plugins.treesitter") 
+  },
+  
+  { 
+    "nvim-telescope/telescope.nvim", 
+    dependencies = { 
+      "nvim-lua/plenary.nvim" 
+    }, 
+    config = require("plugins.telescope") 
+  },
+
+  { "goolord/alpha-nvim" },
+  { "nvim-tree/nvim-web-devicons" },
+  { "marko-cerovac/material.nvim" },
+
+  { 
+    "spiperac/buffhunter.nvim", 
+    config = require("plugins.buffhunter") 
+  },
+
+  -- Noice & Leet
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = require("plugins.noice"),
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      }
+  },
+  {
+      "kawre/leetcode.nvim",
+      build = ":TSUpdate html",
+      dependencies = {
+          "nvim-telescope/telescope.nvim",
+          "nvim-lua/plenary.nvim",
+          "MunifTanjim/nui.nvim",
+          "nvim-treesitter/nvim-treesitter",
+          "nvim-tree/nvim-web-devicons",
+      },  
+  }
+
+
+})
