ba5ef3bbc2a45dde147f143512e4683d7683e200
spiperac <spiperac@denkei.org>
2024-12-24 00:34:12 +0100

    ignore  specific hidden files

--- config/.config/nvim/lua/plugins/telescope.lua
+++ config/.config/nvim/lua/plugins/telescope.lua
@@ -7,6 +7,17 @@ return {
 		config = function()
 			require("telescope").setup({
 				previewer = false,
+				pickers = {
+					find_files = {
+            hidden=true,
+						layout_config = {
+							height = 0.70,
+						},
+					},
+				},
+        defaults = {
+          file_ignore_patterns = {"*.git", ".git/*", "node_modules/*", "target/*"},
+        },
 				extensions = {
 					projects = {
 						detection_methods = { "pattern" },
