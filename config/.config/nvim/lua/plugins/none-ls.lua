1ae2aaf140c01d0df34da63e2affb7e39a410f82
spiperac <spiperac@denkei.org>
2024-12-27 16:53:24 +0100

    Adding ansible

--- config/.config/nvim/lua/plugins/none-ls.lua
+++ config/.config/nvim/lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
       null_ls.setup({
         sources = {
           null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.golines,
         },
       })
       vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
