086f9c1dab81ec803a2f2a489e61e26fd2c7f240
Strahinja Piperac <spiperac@denkei.org>
2025-01-18 04:18:23 +0100

    Function for collecting assets, and properly making pages with my template file.

--- config/.emacs.d/config/website.el
+++ config/.emacs.d/config/website.el
@@ -10,28 +10,47 @@
 (use-package htmlize
   :defer t)
 
-(defun generate-website ()
-    (interactive)
-    (setq org-html-validation-link nil)
-    (setq org-html-head-include-scripts nil)
-    (setq org-html-head-include-default-style nil)
-    (setq org-publish-project-alist
-        (list
-        (list "spiperac-site"
-                :recursive t
-                :base-directory "~/Vault/Web/content"
-                :publishing-function 'org-html-publish-to-html
-                :publishing-directory "~/Vault/Web/public"
-                :with-author nil
-                :with-creator t
-                :with-toc t                ;; Table of content
-                :section-numbers nil
-                :time-stamp-file nil
-                )
-        ))
+(defun copy-theme-assets ()
+  "Copy theme assets to publish directory."
+  (let ((src-dir "~/Vault/Web/spiperac.dev/theme/")
+        (dest-dir "~/Vault/Web/spiperac.dev/public/"))
+    ;; Create assets directory if it doesn't exist
+    (make-directory (concat dest-dir "assets/") t)
+    ;; Copy CSS
+    (copy-file (concat src-dir "style.css")
+               (concat dest-dir "style.css")
+               t)
+    ;; Copy assets directory
+    (dolist (file (directory-files-recursively (concat src-dir "assets/") ".*"))
+      (let* ((relative-path (file-relative-name file src-dir))
+             (dest-path (concat dest-dir relative-path)))
+        (make-directory (file-name-directory dest-path) t)
+        (copy-file file dest-path t)))))
 
-    (org-publish-all t)
-    (message "Building Website complete!"))
+(defun generate-website ()
+  (interactive)
+  (setq org-html-validation-link nil)
+  (setq org-html-head-include-scripts nil)
+  (setq org-html-postamble nil)
+  (setq org-html-head-include-default-style nil)
+  (setq org-html-head (with-temp-buffer
+                       (insert-file-contents "~/Vault/Web/spiperac.dev/theme/template.html")
+                       (buffer-string)))
+  (setq org-publish-project-alist
+        (list
+         (list "spiperac-site"
+               :recursive t
+               :base-directory "~/Vault/Web/spiperac.dev/content"
+               :publishing-function 'org-html-publish-to-html
+               :publishing-directory "~/Vault/Web/spiperac.dev/public"
+               :with-author nil
+               :with-creator t
+               :with-toc t
+               :section-numbers nil
+               :time-stamp-file nil)))
+  (org-publish-all t)
+  (copy-theme-assets)  ; Add this line
+  (message "Building Website complete!"))
 
 (global-set-key (kbd "C-c w") 'generate-website)
 (provide 'website)
