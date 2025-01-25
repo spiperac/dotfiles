4b2872b4be2769c53d3932640d2360929b22f31c
Strahinja Piperac <spiperac@denkei.org>
2025-01-25 08:04:22 +0100

    removed a reference to treemacs.

--- config/.emacs.d/config/custom_fn.el
+++ config/.emacs.d/config/custom_fn.el
@@ -4,21 +4,6 @@
 
 ;;; Code:
 
-;; Treemacs open selected file in vertical split window by pressing C-x vs
-(defun treemacs-open-file-in-vsplit ()
-  "Open the selected file in a vertical split."
-  (interactive)
-  (let ((file (treemacs-copy-filename-at-point)))
-    (if file
-        (progn
-          (delete-window (treemacs-get-local-window))
-          (split-window-right)
-          (find-file file)
-          (treemacs-select-window))
-      (message "No file selected"))))
-
-(global-set-key (kbd "C-x vs") 'treemacs-open-file-in-vsplit)
-
 ;; Kill projectile project and return to dashboard
 (defun quit-project-and-go-to-dashboard ()
   "Kill all project buffers and return to the dashboard."
